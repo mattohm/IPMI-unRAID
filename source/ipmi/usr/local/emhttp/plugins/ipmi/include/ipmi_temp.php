d6227a70c7c20dff2d6d4f5d5eea9f4d208ba3e4
mattohm <31443434+mattohm@users.noreply.github.com>
2019-07-05 11:34:59 -0400

    Update ipmi_temp.php

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_temp.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_temp.php
@@ -62,6 +62,20 @@ if (!empty($disp_sensors)){
             }
         }
     }
+ $bu_date = shell_exec("/usr/bin/stat  /mnt/user/scripts/backupstatus.txt | grep Modify | awk '{print $2}'");
+ $bu_code = shell_exec("/usr/bin/cat  /mnt/user/scripts/backupstatus.txt");
+ $bu_color = 'green';
+ if ($bu_code != 0)
+   $bu_color = 'red';
+ $displays[] = "<span title='Borg'><font color='$bu_color'>$bu_date</font><small>Backup</small></span>";
+
+ $rc_date = shell_exec("/usr/bin/stat  /mnt/user/scripts/rclonestatus.txt | grep Modify | awk '{print $2}'");
+ $rc_code = shell_exec("/usr/bin/cat  /mnt/user/scripts/rclonestatus.txt");
+ $rc_color = 'green';
+ if ($rc_code != 0)
+   $rc_color = 'red';
+ $displays[] = "<span title='Borg'><font color='$bu_color'>$bu_date</font><small>RClone</small></span>";
+
 }
 if ($displays)
     echo "<span id='impitemps' style='margin-right:16px;font-weight: bold;cursor: pointer;'>".implode('&nbsp;', $displays)."</span>";
